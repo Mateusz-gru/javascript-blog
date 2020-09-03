0053f5491e8be112ff4f1bb4dc7f094eb8faa2cb
Mateusz <grudzinski.mateusz4@gmail.com>
2020-09-04 00:34:26 +0200

    Dokończenie handlera eventu
    Niestety nie wiem dlaczego nie pojawia mi się w ogóle tekst artykułu?

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ document.getElementById('test-button').addEventListener('click', function(){
 */
 
 const titleClickHandler = function(event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log('event', event);
@@ -18,7 +19,7 @@ const titleClickHandler = function(event) {
         activeLink.classList.remove('active');
     }
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
 
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
@@ -31,11 +32,22 @@ const titleClickHandler = function(event) {
         activeArticle.classList.remove('active');
     }
   
-    /* get 'href' attribute from the clicked link */
-  
+    /* [IN PROGRESS] get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute('href');
+    
+    console.log(articleSelector);
+
     /* find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+
+    console.log(targetArticle);
   
     /* add class 'active' to the correct article */
+    
+    article.classList.add('active');
+
 }
   
 const links = document.querySelectorAll('.titles a');
